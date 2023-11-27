68d03397943ab06039a8e2144bea0a3252d97181
leoemcasa <leoemcasa@hotmail.com>
2023-11-27 13:07:23 -0300

    node midleware bodyParser

--- 24-expressJs/4-middleware/index1.js
+++ 24-expressJs/4-middleware/index1.js
@@ -1,10 +1,18 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { parseArgs } from "util";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
