4e4c5d6cd5307c79c35fa3aa794cbc311eb8b677
leoemcasa <leoemcasa@hotmail.com>
2023-12-01 12:16:38 -0300

    json Taco fim

--- 28-api/2-json/index.js
+++ 28-api/2-json/index.js
@@ -12,26 +12,29 @@ const recipeJSON =
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let recipe = JSON.parse(recipeJSON)
-
+let recipeFilteredJSON;
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { recipe: recipeFilteredJSON });
 });
 
 app.post("/recipe", (req, res) => {
-  let recip
-  console.log(req.body.choice);
-  if (req.body.choice == "chicken") {
-    recip = recipe[0];
-  } else if (req.body.choice == "beef") {
-    recip = recipe[1];
-  } else {
-    recip = recipe[2]
-  }
-  // console.log(recipe);
+
   //Step 3: Write your code here to make this behave like the solution website.
   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
-  res.render("index.ejs", { recipe: recip });
+  switch (req.body.choice) {
+    case "chicken":
+      recipeFilteredJSON = JSON.parse(recipeJSON)[0];
+      break;
+    case "beef":
+      recipeFilteredJSON = JSON.parse(recipeJSON)[1];
+      break;
+    case "fish":
+      recipeFilteredJSON = JSON.parse(recipeJSON)[2];
+      break;
+    default:
+      break;
+  }
+  res.redirect("/");
 });
 
 app.listen(port, () => {
