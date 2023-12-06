aa490c074986f321ed62053e2d7d77dd510ad216
leoemcasa <leoemcasa@hotmail.com>
2023-12-06 16:08:12 -0300

    PostgreSQL traveltracker

--- 33-PostgreSQL/2postgresRead/index.js
+++ 33-PostgreSQL/2postgresRead/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let currentQuestion = {};
+let quiz;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "192.168.15.66",
+  database: "world",
+  password: "secretPG",
+  port: 5432,
+});
+
+db.connect();
+
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
+
 
 // GET home page
 app.get("/", (req, res) => {
@@ -24,7 +46,7 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
---
+++ 33-PostgreSQL/world-food.sql
@@ -0,0 +1,6 @@
+CREATE TABLE world_food (
+	id SERIAL PRIMARY KEY,
+	Country VARCHAR(20),
+	Rice FLOAT,
+	Wheat FLOAT
+);
