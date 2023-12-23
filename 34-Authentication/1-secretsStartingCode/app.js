b73470e78ee5cea60abe1959a7312ac71d9cb2cf
leoemcasa <leoemcasa@hotmail.com>
2023-12-22 21:01:13 -0300

    Auth level2

--- 34-Authentication/1-secretsStartingCode/app.js
+++ 34-Authentication/1-secretsStartingCode/app.js
@@ -1,1 +1,65 @@
 //jshint esversion:6
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import encrypt from "mongoose-encryption";
+
+dotenv.config();
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const uri = `mongodb+srv://sales:${process.env.MONGODB_ATLAS}@cluster0.p2nlsvv.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(uri);
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, { secret: process.env.CRYPT_SECRET, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+})
+
+app.get("/login", function(req, res){
+    res.render("login.ejs");
+})
+
+app.get("/register", function(req, res){
+    res.render("register.ejs");
+})
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save();
+    res.render("secrets.ejs");
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}).then((foundUser) => {
+        if (foundUser.password == password) {
+            res.render("secrets.ejs");
+        } else {
+            res.render("login.ejs")
+        };
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+  
