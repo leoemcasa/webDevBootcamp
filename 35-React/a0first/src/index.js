84f63ac6002d4868ef1e76809e6b283fc1e280b3
leoemcasa <leoemcasa@hotmail.com>
2024-01-19 17:19:54 -0300

    React a0

---
+++ 35-React/a0first/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Hello World!</h1>
+  </React.StrictMode>
+);
+
