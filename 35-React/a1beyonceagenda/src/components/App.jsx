a2617ed7de1d4f29085b881de34ee0d594872d49
leoemcasa <leoemcasa@hotmail.com>
2024-01-22 12:11:48 -0300

    React a1beyondeagendaFim

--- 35-React/a1beyonceagenda/src/components/App.jsx
+++ 35-React/a1beyonceagenda/src/components/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function createCard(contacts) {
+    return (<Card 
+        key = {contacts.id}
+        id = {contacts.id}
+        name = {contacts.name}
+        imgURL = {contacts.imgURL}
+        phone = {contacts.phone}
+        email = {contacts.email}
+
+    />);
+}
 
 function App(props) {
   return (
     <div>
+      <Avatar imgURL="https://lh3.googleusercontent.com/a/ACg8ocITRU1LqsXe7J3hk3NEflJQyJj7OT_gjV9hI2wpYSs2XQg=s360-c-no" />
       <h1 className="heading">My Contacts</h1>
-      <Card
+      {contacts.map(createCard)}
+      {/* <Card
         name={contacts[0].name}
         imgURL={contacts[0].imgURL}
         phone={contacts[0].phone}
@@ -23,7 +38,7 @@ function App(props) {
         imgURL={contacts[2].imgURL}
         phone={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
--- 35-React/a1beyonceagenda/src/components/Card.jsx
+++ 35-React/a1beyonceagenda/src/components/Card.jsx
@@ -1,18 +1,18 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
 
 function Card(props) {
     return (
       <div className="card">
         <div className="top">
+          <p>{props.id}</p>
           <h2 className="name">{props.name}</h2>
-          <img className="circle-img"
-            src={props.imgURL}
-            alt="avatar_img"
-          />
+          <Avatar imgURL={props.imgURL} />
         </div>
         <div className="bottom">
-          <p className="info">{props.phone}</p>
-          <p className="info">{props.email}</p>
+          <Detail detail={props.phone} />
+          <Detail detail={props.email} />
         </div>
       </div>
     );
