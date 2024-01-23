149ea5ba8b6432a3146b21e0b3b08424791909e8
leoemcasa <leoemcasa@hotmail.com>
2024-01-23 11:42:28 -0300

    React a0Keeper Map e props

--- 35-React/a0keeper/src/components/App.jsx
+++ 35-React/a0keeper/src/components/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(e => (
+                <Note 
+                    key = {e.key}
+                    title = {e.title}
+                    content = {e.content}
+                />
+            ))}
             <Footer />
         </div>
     );
