44ac8ac1985272ef2cc791ca5e83a929e4b687a2
leoemcasa <leoemcasa@hotmail.com>
2024-01-24 13:47:56 -0300

    React a2 conditiona rendering PracticeEslint

--- 35-React/a2conditionalrenderingpratice/src/components/App.jsx
+++ 35-React/a2conditionalrenderingpratice/src/components/App.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import Form from "./Form";
+import React from 'react';
+import Form from './Form';
 
-var userIsRegistered = true;
+const userIsRegistered = true;
 
 function App() {
   return (
     <div className="container">
-      <Form userIsRegistered = {userIsRegistered} />
+      <Form userIsRegistered={userIsRegistered} />
     </div>
   );
 }
 
 export default App;
 
-//Challenge: Without moving the userIsRegistered variable,
-//1. Show Login as the button text if userIsRegistered is true.
-//Show Register as the button text if userIsRegistered is false.
-//2. Only show the Confirm Password input if userIsRegistered is false.
-//Don't show it if userIsRegistered is true.
+// Challenge: Without moving the userIsRegistered variable,
+// 1. Show Login as the button text if userIsRegistered is true.
+// Show Register as the button text if userIsRegistered is false.
+// 2. Only show the Confirm Password input if userIsRegistered is false.
+// Don't show it if userIsRegistered is true.
